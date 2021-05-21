10131803332014923f2e78ac711695449d563e60
DaltonHart <daltonvaren@gmail.com>
2021-05-21 11:34:32 -0700

    adds base of nav with user props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from "./components/Nav/Nav";
+
+import "./App.css";
 
 function App() {
+  const user = {
+    username: "Dalton",
+    avatar:
+      "https://static-cdn.jtvnw.net/jtv_user_pictures/0ed35a26-2358-48d7-a03d-a7bf4b08a64b-profile_image-70x70.png",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Nav user={user} />
     </div>
   );
 }
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,37 @@
+import "./Nav.css";
+
+function Nav(props) {
+  const { username, avatar } = props.user;
+
+  return (
+    <header className='header'>
+      <nav className='header__left'>
+        <span className='icon'>
+          <img
+            src='https://static.twitchcdn.net/assets/favicon-32-e29e246c157142c94346.png'
+            alt='logo'
+          />
+        </span>
+        <a href='/' className='nav__item'>
+          Following
+        </a>
+        <a href='/' className='nav__item'>
+          Browse
+        </a>
+        <a href='/' className='nav__item'>
+          Esports
+        </a>
+        <a href='/' className='nav__item'>
+          Music
+        </a>
+      </nav>
+      <nav className='header__right'>
+        <span className='user'>
+          <img src={avatar} alt='user avatar' />
+        </span>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
